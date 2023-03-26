57864d36d3204628f855f1fc2cf7d62c8dbdd48a
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2023-03-27 00:53:01 +0200

    aa

--- src/main/java/gov/iti/jets/filters/AdminFilter.java
+++ src/main/java/gov/iti/jets/filters/AdminFilter.java
@@ -1,8 +1,13 @@
 package gov.iti.jets.filters;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+
+import gov.iti.jets.persistent.dto.UserDTO;
 
 public class AdminFilter implements Filter {
 
@@ -14,6 +19,22 @@ public class AdminFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("admin filter");
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession(false);
+        int userId = -1;
+
+        if (session != null) {
+            UserDTO user = (UserDTO) session.getAttribute("userSession");
+
+            if (user != null) {
+                if(!user.isAdmin()){
+                    ((HttpServletResponse) response).sendRedirect("/petpet/home");
+                    return;
+                }
+            }
+
+        }
 
         chain.doFilter(request, response);
     }
