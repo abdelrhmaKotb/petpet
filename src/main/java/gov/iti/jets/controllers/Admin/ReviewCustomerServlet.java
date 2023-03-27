c9f11231c27b68f0bafb670926cf6cf218d497ef
mohgayasser <mohgayasserelsaid@gmail.com>
2023-03-28 01:18:23 +0200

    user list

--- src/main/java/gov/iti/jets/controllers/Admin/ReviewCustomerServlet.java
+++ src/main/java/gov/iti/jets/controllers/Admin/ReviewCustomerServlet.java
@@ -1,6 +1,10 @@
 package gov.iti.jets.controllers.Admin;
 
+import com.google.gson.Gson;
+import gov.iti.jets.persistent.dto.OrderDto;
 import gov.iti.jets.persistent.dto.UserDTO;
+import gov.iti.jets.persistent.entity.User;
+import gov.iti.jets.services.GetOrdersServices;
 import gov.iti.jets.services.GetUsersService;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -9,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class ReviewCustomerServlet extends HttpServlet {
@@ -16,15 +21,21 @@ public class ReviewCustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        GetUsersService usersService = new GetUsersService();
+        GetUsersService getUsersService =new GetUsersService();
+        List<UserDTO> userDTOS =  getUsersService.getAllUsersByPageNumber(0);
+        long totalPages = getUsersService.totalUsers( );
 
-        List<UserDTO> users = usersService.getAllUsers();
-
-        req.setAttribute("AllUsers" , users);
-
+        req.setAttribute("AllUsers" , userDTOS);
+        req.setAttribute("totalPages", Math.ceil( (double)totalPages/5));
         req.getRequestDispatcher("user-list").forward(req , resp);
+    }
 
-
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pageNumber= req.getParameter("pageNumber");
+        PrintWriter printWriter =resp.getWriter();
+        GetUsersService getUsersService =new GetUsersService();
+        List<UserDTO> userDTOS =  getUsersService.getAllUsersByPageNumber( Integer.valueOf(pageNumber)-1);
+        printWriter.write(new Gson().toJson(userDTOS));
     }
 }
